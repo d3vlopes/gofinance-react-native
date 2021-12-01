fdeacc5cab4685216bc73f7242c6aea1807108db
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-12-01 19:50:13 -0300

    feat: add Button

---
+++ src/components/Forms/Button/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { TouchableOpacityProps } from 'react-native'
+
+import * as S from './styles'
+
+type ButtonProps = {
+  title: string
+} & TouchableOpacityProps
+
+export function Button({ title, ...rest }: ButtonProps) {
+  return (
+    <S.Container>
+      <S.Title>{title}</S.Title>
+    </S.Container>
+  )
+}
