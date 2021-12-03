4afc1544a2fe0d1f68de826a7685d991a9f5b26e
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-12-03 20:18:29 -0300

    feat: add loading on Dashboard screen

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect, useCallback } from 'react'
+import { ActivityIndicator } from 'react-native'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { useFocusEffect } from '@react-navigation/native'
+import { useTheme } from 'styled-components'
 
 import { HightlightCard } from '../../components/HighlightCard'
 import {
@@ -27,10 +29,13 @@ type HiglightData = {
 }
 
 export function Dashboard() {
+  const [isLoading, setIsLoading] = useState(true)
   const [transactions, setTransactions] = useState<DataListProps[]>([])
   const [highlightData, setHighlightData] = useState<HiglightData>(
     {} as HiglightData,
   )
+
+  const theme = useTheme()
 
   async function laodTransactions() {
     const dataKey = '@gofinances:transactions'
@@ -76,6 +81,7 @@ export function Dashboard() {
         amount: formatAmount(String(total)),
       },
     })
+    setIsLoading(false)
   }
 
   useEffect(() => {
@@ -88,52 +94,66 @@ export function Dashboard() {
     }, []),
   )
 
+  const renderLoading = () => {
+    return (
+      <S.LoadingContainer>
+        <ActivityIndicator color={theme.colors.primary} size="large" />
+      </S.LoadingContainer>
+    )
+  }
+
+  const renderContent = () => {
+    return (
+      <>
+        <S.Header>
+          <S.UserWrapper>
+            <S.UserInfo>
+              <S.Photo
+                source={{
+                  uri: 'https://avatars.githubusercontent.com/u/59663666?v=4',
+                }}
+              />
+              <S.User>
+                <S.UserGretting>Olá,</S.UserGretting>
+                <S.UserName>Leandro</S.UserName>
+              </S.User>
+            </S.UserInfo>
+            <S.Icon name="power" />
+          </S.UserWrapper>
+        </S.Header>
+        <S.HighlightCardWrapper>
+          <HightlightCard
+            type="up"
+            title="Entradas"
+            amount={highlightData.entries.amount}
+            lastTransaction="Última entrada dia 13 de abril"
+          />
+          <HightlightCard
+            type="down"
+            title="Saidas"
+            amount={highlightData.expensives.amount}
+            lastTransaction="Última entrada dia 03 de abril"
+          />
+          <HightlightCard
+            type="total"
+            title="Total"
+            amount={highlightData.total.amount}
+            lastTransaction="01 à 16 de abril"
+          />
+        </S.HighlightCardWrapper>
+        <S.Transactions>
+          <S.Title>Listagem</S.Title>
+          <S.TransactionList
+            data={transactions}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => <TransactionCard data={item} />}
+          />
+        </S.Transactions>
+      </>
+    )
+  }
+
   return (
-    <S.Container>
-      <S.Header>
-        <S.UserWrapper>
-          <S.UserInfo>
-            <S.Photo
-              source={{
-                uri: 'https://avatars.githubusercontent.com/u/59663666?v=4',
-              }}
-            />
-            <S.User>
-              <S.UserGretting>Olá,</S.UserGretting>
-              <S.UserName>Leandro</S.UserName>
-            </S.User>
-          </S.UserInfo>
-          <S.Icon name="power" />
-        </S.UserWrapper>
-      </S.Header>
-      <S.HighlightCardWrapper>
-        <HightlightCard
-          type="up"
-          title="Entradas"
-          amount={highlightData.entries.amount}
-          lastTransaction="Última entrada dia 13 de abril"
-        />
-        <HightlightCard
-          type="down"
-          title="Saidas"
-          amount={highlightData.expensives.amount}
-          lastTransaction="Última entrada dia 03 de abril"
-        />
-        <HightlightCard
-          type="total"
-          title="Total"
-          amount={highlightData.total.amount}
-          lastTransaction="01 à 16 de abril"
-        />
-      </S.HighlightCardWrapper>
-      <S.Transactions>
-        <S.Title>Listagem</S.Title>
-        <S.TransactionList
-          data={transactions}
-          keyExtractor={(item) => item.id}
-          renderItem={({ item }) => <TransactionCard data={item} />}
-        />
-      </S.Transactions>
-    </S.Container>
+    <S.Container>{isLoading ? renderLoading() : renderContent()}</S.Container>
   )
 }
