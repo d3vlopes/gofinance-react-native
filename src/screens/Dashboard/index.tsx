9ef369998c23e96faa0f1a75320729f9c39ce657
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-12-03 16:27:08 -0300

    feat: make cards dynamic of Dashboard screen

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -16,16 +16,38 @@ export interface DataListProps extends TransactionCardProps {
   id: string
 }
 
+type HighlightProps = {
+  amount: string
+}
+
+type HiglightData = {
+  entries: HighlightProps
+  expensives: HighlightProps
+  total: HighlightProps
+}
+
 export function Dashboard() {
-  const [data, setData] = useState<DataListProps[]>([])
+  const [transactions, setTransactions] = useState<DataListProps[]>([])
+  const [highlightData, setHighlightData] = useState<HiglightData>(
+    {} as HiglightData,
+  )
 
   async function laodTransactions() {
     const dataKey = '@gofinances:transactions'
     const response = await AsyncStorage.getItem(dataKey)
     const transactions = response ? JSON.parse(response) : []
 
+    let entriesTotal = 0
+    let expensiveTotal = 0
+
     const transactionsFormatted: DataListProps[] = transactions.map(
       (item: DataListProps) => {
+        if (item.type === 'positive') {
+          entriesTotal += Number(item.amount)
+        } else {
+          expensiveTotal += Number(item.amount)
+        }
+
         const amount = formatAmount(item.amount)
         const date = formatDate(item.date)
 
@@ -40,7 +62,20 @@ export function Dashboard() {
       },
     )
 
-    setData(transactionsFormatted)
+    const total = entriesTotal - expensiveTotal
+
+    setTransactions(transactionsFormatted)
+    setHighlightData({
+      entries: {
+        amount: formatAmount(String(entriesTotal)),
+      },
+      expensives: {
+        amount: formatAmount(String(expensiveTotal)),
+      },
+      total: {
+        amount: formatAmount(String(total)),
+      },
+    })
   }
 
   useEffect(() => {
@@ -75,26 +110,26 @@ export function Dashboard() {
         <HightlightCard
           type="up"
           title="Entradas"
-          amount="R$ 17.400,00"
+          amount={highlightData.entries.amount}
           lastTransaction="Última entrada dia 13 de abril"
         />
         <HightlightCard
           type="down"
           title="Saidas"
-          amount="R$ 1.259,00"
+          amount={highlightData.expensives.amount}
           lastTransaction="Última entrada dia 03 de abril"
         />
         <HightlightCard
           type="total"
           title="Total"
-          amount="R$ 16.141,00"
+          amount={highlightData.total.amount}
           lastTransaction="01 à 16 de abril"
         />
       </S.HighlightCardWrapper>
       <S.Transactions>
         <S.Title>Listagem</S.Title>
         <S.TransactionList
-          data={data}
+          data={transactions}
           keyExtractor={(item) => item.id}
           renderItem={({ item }) => <TransactionCard data={item} />}
         />
