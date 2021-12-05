20bde643cab6fceafb734bcaa27fa5888beee496
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-12-04 23:56:32 -0300

    feat: add Routes for handle routes public and private

--- App.tsx
+++ App.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { StatusBar } from 'react-native'
 import AppLoading from 'expo-app-loading'
-import { NavigationContainer } from '@react-navigation/native'
 import { ThemeProvider } from 'styled-components'
 import {
   useFonts,
@@ -12,10 +11,9 @@ import {
 
 import { AuthProvider } from './src/hooks/auth'
 
-import theme from './src/global/styles/theme'
+import { Routes } from './src/routes'
 
-import { AppRoutes } from './src/routes/app.routes'
-import { SignIn } from './src/screens/SignIn'
+import theme from './src/global/styles/theme'
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -30,13 +28,10 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <NavigationContainer>
-        <StatusBar barStyle="light-content" />
-        {/* <AppRoutes /> */}
-        <AuthProvider>
-          <SignIn />
-        </AuthProvider>
-      </NavigationContainer>
+      <StatusBar barStyle="light-content" />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
     </ThemeProvider>
   )
 }
