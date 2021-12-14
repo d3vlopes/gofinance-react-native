639287e6dc9da8f5914c660d1946b6295d4bc9b1
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-12-14 15:11:43 -0300

    chore: add setup for test

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  preset: 'jest-expo',
+  testPathIgnorePatterns: ['/node_modules', '/android', '/ios'],
+  setupFilesAfterEnv: [
+    '@testing-library/jest-native/extend-expect',
+  ],
+}
